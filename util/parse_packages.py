1904813bb0139b3baace0b1e228d1ef7c01decb9
Tatyana Voloshina <voloshina@selectel.ru>
2020-12-18 14:25:04 +0300

    Change file open mode

--- util/parse_packages.py
+++ util/parse_packages.py
@@ -12,13 +12,13 @@ for pkgfile in sys.argv[1:]:
     if pkgfile.endswith(".gz"):
         import gzip
 
-        file = gzip.open(pkgfile)
+        file = gzip.open(pkgfile, mode='rt', encoding="utf-8")
     elif pkgfile.endswith(".bz2"):
         import bz2
 
-        file = bz2.BZ2File(pkgfile)
+        file = bz2.open(pkgfile, mode='rt', encoding="utf-8")
     else:
-        file = open(pkgfile)
+        file = open(pkgfile, mode='rt', encoding="utf-8")
 
     # Current package 
     pkg = {}
